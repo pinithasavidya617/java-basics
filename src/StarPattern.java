4037390ce5d814d8513924622827afc634ff7960
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-03-04 09:55:20 +0530

    Add inverted pyramid

--- src/StarPattern.java
+++ src/StarPattern.java
@@ -5,8 +5,11 @@ public class StarPattern {
 //        pattern();
 //        pattern2();
 //        pattern3();
-        userPattern();
+//        userPattern();
+//        pyramid();
+        invertedPyramid();
     }
+
 
     private static void pattern(){
         for(int i = 1; i <= 5; i ++){     //rows
@@ -60,4 +63,40 @@ public class StarPattern {
         scanner.close();
 
     }
+
+    public static void pyramid(){
+        int n = 5;
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= (2*i-1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void invertedPyramid(){
+        int n = 5;
+
+        for(int i = 5; i >= 1; i--) {
+
+            for(int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= (2*i-1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
 }
+
+
+
