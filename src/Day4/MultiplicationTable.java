f8d682b0eb7150d729a66799d4199625986ec8d3
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-03-09 15:13:15 +0530

    Add multiplication table

---
+++ src/Day4/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        printTable(getNumber());
+    }
+    public static int getNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        return sc.nextInt();
+    }
+
+    public static void printTable(int n){
+        for(int i = 1; i <= 12; i++){
+            System.out.println(n + " * " + i + " = " + (n * i));
+        }
+    }
+}
