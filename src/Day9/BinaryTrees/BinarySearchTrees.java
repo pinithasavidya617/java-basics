08fb0f51d8b541541e6bc16c05df3ccc03e6d4c8
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-04-26 16:04:12 +0530

    Add binary search tree 4/22

--- src/Day9/BinaryTrees/BinarySearchTrees.java
+++ src/Day9/BinaryTrees/BinarySearchTrees.java
@@ -10,19 +10,87 @@ public class BinarySearchTrees {
         this.root = null;
     }
 
+    // without recursive mode
+//    public void insert(int value) {
+//
+//        if (root == null) {
+//            this.root = new Node(value);
+//        }
+//
+//        if (root.data > value) {
+//            root.left = new Node(value);
+//        }
+//
+//        if(root.data < value) {
+//            root.right = new Node(value);
+//        }
+//    }
+
+
+    // recursive mode
     public void insert(int value) {
 
-        if (root == null) {
-            root = new Node(value);
+        root = insertHelper(root , value);
+    }
 
-        }else {
-            if (root.data > value) {
-                root.left = new Node(value);
+    private Node insertHelper(Node node , int value) {
 
-            }else if(root.data < value) {
-                root.right = new Node(value);
-            }
+        if (node == null) {
+            return new Node(value);
         }
 
+        if (value < node.data) {
+            node.left = insertHelper(node.left , value);
+
+        }else if (value > node.data) {
+            node.right = insertHelper(node.right , value);
+        }
+
+        return node;
+    }
+
+    public void inOrder () {
+        inOrderHelper(root);
+    }
+
+    public void inOrderHelper (Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        inOrderHelper(node.left);
+        System.out.println(node.data + " ");
+        inOrderHelper(node.right);
+    }
+
+    public void preOrder() {
+        preOrderHelper(root);
+    }
+
+    public void preOrderHelper (Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.data + " ");
+        preOrderHelper(node.left);
+        preOrderHelper(node.right);
+    }
+
+    public void postOrder() {
+        postOrderHelper(root);
+    }
+
+    public void postOrderHelper (Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        postOrderHelper(node.left);
+        postOrderHelper(node.right);
+        System.out.println(node.data + " ");
     }
 }
--- src/Day9/BinaryTrees/Main.java
+++ src/Day9/BinaryTrees/Main.java
@@ -1,14 +1,26 @@
 package Day9.BinaryTrees;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
         BinarySearchTrees bst = new BinarySearchTrees();
 
-        bst.insert(10) ;
-        bst.insert(20);
+        bst.insert(40);
         bst.insert(30);
+        bst.insert(60);
+        bst.insert(15);
+        bst.insert(35);
+        bst.insert(80);
+        bst.insert(50);
+        bst.insert(45);
+        bst.insert(70);
 
+        bst.inOrder();
+        System.out.println("_____________________");
+        bst.preOrder();
+        System.out.println("_____________________");
+        bst.postOrder();
     }
 }
